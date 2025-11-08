d718cbe698cdeef5f0baae0451a5f58809304406
FahriX3 <fahrimandriva138@gmail.com>
2025-11-08 15:00:43 +0700

    add file

---
+++ WebApplicationTest/WebApplicationTest/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet("halo")]
+        public string Index()
+        {
+            return "hello selamat pagi";
+        }
+
+        [HttpGet("test")]
+        public string nama([FromQuery] string nama)
+        {
+            return "hello, " + nama ;
+        }
+    }
+}
